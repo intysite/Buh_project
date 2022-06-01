45a236eab8226b7f36d19516a08a784e96992618
Yuri Kulikov <intysite@gmail.com>
2022-06-02 01:30:14 +0300

    Необходимые правки

--- src/First_homework.java
+++ src/First_homework.java
@@ -1,6 +1,5 @@
 public class First_homework {
     public static void main(String[] args) {
         System.out.println("Первое занятие - огонь!");
-        System.out.print("Spat idi, umnik");
     }
 }
